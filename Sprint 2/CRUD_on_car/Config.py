04af6956cb5c0d06edea352ce2b7f4ba08d68c2d
Atai Askarov <101080966+Atai-Askarov@users.noreply.github.com>
2024-02-29 09:27:37 -0500

    Add files via upload

---
+++ Sprint 2/CRUD_on_car/Config.py
@@ -0,0 +1,34 @@
+import os
+'''
+username = "sql5686988"
+password = "jSrGqLGIWE"
+host = "sql5.freemysqlhosting.net"
+port = "3306"
+dbname = "sql5686988"
+db_url = f"mysql+pymysql://{username}:{password}@{host}:{port}/{dbname}"
+'''
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DEV_DATABASE_URL", "mysql+pymysql://sql5686988:jSrGqLGIWE@sql5.freemysqlhosting.net:3306/sql5686988")
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URL", "sqlite:///:memory:")
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://sql5686988:jSrGqLGIWE@sql5.freemysqlhosting.net:3306/sql5686988")
+
+config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+    "default": DevelopmentConfig
+}
