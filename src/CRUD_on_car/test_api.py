a96ec95c8df75f0c54e2dce83a53b477b22d0cff
Atai Askarov <101080966+Atai-Askarov@users.noreply.github.com>
2024-03-11 01:35:16 -0400

    Update test_api.py

--- src/CRUD_on_car/test_api.py
+++ src/CRUD_on_car/test_api.py
@@ -1,6 +1,6 @@
 import pytest, requests
-from CRUD_on_car.Vehicles_CRUD import app1
-from CRUD_on_car.VehiclesDB import db
+from Vehicles_CRUD import app1
+from VehiclesDB import db
 
 
 ENDPOINT = "http://127.0.0.1:5000"
