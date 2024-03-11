58f1acdfd558c134a410dab8510cd160fba457d6
Atai Askarov <101080966+Atai-Askarov@users.noreply.github.com>
2024-03-11 12:21:09 -0400

    Update Vehicles_model.py

--- src/CRUD_on_car/Vehicles_model.py
+++ src/CRUD_on_car/Vehicles_model.py
@@ -40,21 +40,3 @@ class Car(db.Model):
             "price" : self.price
 
         }
-if(__name__) == "__main__":
-    def test_new_Vehicle():
-        """
-        GIVEN a Car model
-        WHEN a new Car is created
-        THEN check the vehicle id, the model name, number of seat, features, and the make name. Assert that they are defined correctly
-        """
-        car = Car(1234, 'Batmobile', 2, "throws explosives", "whatmakesmakes", 2012, 1,"2024-02-05","2024-03-05", 505  )
-        assert car.vehicule_id == 1234
-        assert car.model_name != 'Batmobile'
-        assert car.seats == 2
-        assert car.features == 'throws explosives'
-        assert car.make_name == 'whatmakesmakes'
-        assert car.model_year == 2012
-        assert car.availability == 1
-        assert car.availability_start_date == "2024-02-05"
-        assert car.availability_end_date == "2024-03-05"
-        assert car.price == 505
