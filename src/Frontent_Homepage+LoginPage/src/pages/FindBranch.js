c284c7cae534928cd3fb871388ea132d73f16eea
ltaalba <linataalba@hotmail.com>
2024-03-21 22:17:32 -0400

    check out completed + changed style for find a branch to make everything more homogeneous

--- src/Frontent_Homepage+LoginPage/src/pages/FindBranch.js
+++ src/Frontent_Homepage+LoginPage/src/pages/FindBranch.js
@@ -1,50 +1,43 @@
 import React, { useState } from "react";
-import "../styles/FindBranch.css";
+import "../styles/Login.css"; 
 import CarCard from "../components/CarCard";
 
 function FindBranch() {
-  const [postalCode, setPostalCode] = useState(""); // State for the postal code
-  const [showMessage, setShowMessage] = useState(false); // State to control message visibility
+  const [postalCode, setPostalCode] = useState(""); 
+  const [showMessage, setShowMessage] = useState(false); 
 
-  const submitPress =() => {
-
+  const submitPress = () => {
     window.location.href = "/home";
-        
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (postalCode.trim() !== "") {
-      // If postal code is entered, display the message
+      
       setShowMessage(true);
     }
-
-    
-    
-  }
-  
-  ;
+  };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className="containerHomeee">
-        <div className="findcartext">find a car near you</div>
-        <div className="inputss">
-          <div className="veryveryspecific">
-            <input
-              type="text"
-              placeholder="Enter postal code"
-              value={postalCode}
-              onChange={(e) => setPostalCode(e.target.value)}
-            />
-          </div>
+    <div className="container"> 
+      <form onSubmit={handleSubmit} className="inputs"> 
+        <div className="header"> 
+          <div className="text">Find a branch</div> 
+          <div className="underline"></div> 
         </div>
-        <div className="submit-container">
-          <button type="submit" className="submitButton" onClick={submitPress}>
+        <div className="input">
+          <input
+            type="text"
+            placeholder="Enter postal code"
+            value={postalCode}
+            onChange={(e) => setPostalCode(e.target.value)}
+          />
+        </div>
+        <div className="submit-container"> 
+          <button type="submit" className="submit" onClick={submitPress}> 
             Find a car near me
           </button>
         </div>
-        
       </form>
     </div>
   );
